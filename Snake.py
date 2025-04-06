ed604e8ad6dde1f83dec6f75942fe1fe8ad35361
Ernest-Kalmykov <0.7.07@mail.ru>
2025-04-06 14:46:19 +0300

    Improvements
    Improvements

--- Snake.py
+++ Snake.py
@@ -2,9 +2,6 @@ import pygame
 import time
 import random
 
-from main import MainWindow
-
-
 # Так как у меня проблемы с классами, их нет будет. Просто глобальные переменные
 
 class GameLogics:
@@ -13,65 +10,88 @@ class GameLogics:
     SIZE_COLOR = 20
 
 
-    def __init__(self):
+    def __init__(self, window_x, window_y):
         self.snake_speed = 15
         self.map_color = pygame.Color(0, 0, 0)
         self.color_snake = pygame.Color(0, 255, 0)
         self.snake_position = [100, 50]
         self.SCORE_COLOR = pygame.Color(255, 255, 255)
         self.snake_body = [[100, 50], [90, 50], [80, 50]]
-        self.mouse_position = [random.randrange(1, (MainWindow().WINDOW_X // 10)) * 10,
-                          random.randrange(1, (MainWindow().WINDOW_Y // 10)) * 10]
+        self.window_x = window_x
+        self.window_y = window_y
+        self.mouse_position = [random.randrange(1, (window_x().WINDOW_X // 10)) * 10,
+                               random.randrange(1, (window_y().WINDOW_Y // 10)) * 10]
         self.fruit_spawn = True
         self.score = 0
         self.direction = 'RIGHT'
+        self.change_to = self.direction
 
-    def show_score(self, color, font, size):
-        color = self.SCORE_COLOR# Функция счёта
+    def show_score(self, window, color, font, size):
+        # color = self.SCORE_COLOR# Функция счёта
         score_font = pygame.font.SysFont(font, size)
         score_surface = score_font.render('Счёт: ' + str(self.score), True, color)
-        MainWindow().init_window().blit(score_surface, (10, 10))
+        window.blit(score_surface, (10, 10))
 
-    def game_over(self):  # Функция завершения игры
+    def game_over(self, window):  # Функция завершения игры
         my_font = pygame.font.SysFont('times new roman', 50)
-        game_over_surface = my_font.render('Игра окончена Счёт: ' + str(self.score), True, GameLogics.LOSING_LABEL_COLOR)
+        game_over_surface = my_font.render('Игра окончена Счёт: ' + str(self.score), True, self.LOSING_LABEL_COLOR)
         game_over_rect = game_over_surface.get_rect()
-        game_over_rect.midtop = (MainWindow().WINDOW_X/ 2, MainWindow().WINDOW_Y / 4)
-        MainWindow().init_window().blit(game_over_surface, game_over_rect)
+        game_over_rect.midtop = (self.window_x / 2, self.window_y / 4)
+        window.blit(game_over_surface, game_over_rect)
         pygame.display.flip()
         time.sleep(2)
-        pygame.quit()
-        quit()
+        return "menu"
 
     def body_enlargement(self):
         self.snake_body.insert(0, list(self.snake_position))  # Как змея ест мышей
 
     def eat_mouse(self):
-        self.body_enlargement()
         if self.snake_position[0] == self.mouse_position[0] and self.snake_position[1] == self.mouse_position[1]:
             self.score += 10
             self.fruit_spawn = False
+            self.body_enlargement()
         else:
             self.snake_body.pop()
 
-    def render_snake(self):
+    def render_snake(self, window):
         for pos in self.snake_body:
-            pygame.draw.rect(MainWindow().init_window(), self.color_snake, pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(window, self.color_snake, pygame.Rect(pos[0], pos[1], 10, 10))
 
-    def render_mouse(self):
-        pygame.draw.rect(MainWindow().init_window(), self.SCORE_COLOR, pygame.Rect(self.mouse_position[0], self.mouse_position[1], 10, 10))
+    def render_mouse(self, window):
+        pygame.draw.rect(window, self.MOUSE_COLOR, pygame.Rect(self.mouse_position[0], self.mouse_position[1], 10, 10))
 
     # TODO В будущем сделать перевод змеи через концы экрана (должно зависить от сложности)
     def snake_translation(self):
-        if self.snake_position[0] < 0 or self.snake_position[0] > MainWindow().WINDOW_X - 10:
-            self.game_over()
-        if self.snake_position[1] < 0 or self.snake_position[1] > MainWindow().WINDOW_Y - 10:
-            self.game_over()
+        if self.snake_position[0] < 0 or self.snake_position[0] > self.window_x - 10:
+            return True
+        if self.snake_position[1] < 0 or self.snake_position[1] > self.window_y - 10:
+            return True
+        return False
 
     def suicide(self):
         for block in self.snake_body[1:]:
             if self.snake_position[0] == block[0] and self.snake_position[1] == block[1]:
-                self.game_over()
+                return True
+        return False
+
+    def update_position(self):
+        if self.change_to == "UP" and self.direction != "DOWN":
+            self.direction = "UP"
+        if self.change_to == "DOWN" and self.direction != "UP":
+            self.direction = "DOWN"
+        if self.change_to == "LEFT" and self.direction != "RIGHT":
+            self.direction = "LEFT"
+        if self.change_to == "RIGHT" and self.direction != "LEFT":
+            self.direction = "RIGHT"
+
+        if self.direction == "UP":
+            self.snake_position[1] -= 10
+        if self.direction == "DOWN":
+            self.snake_position[1] += 10
+        if self.direction == "LEFT":
+            self.snake_position[1] -= 10
+        if self.direction == "RIGHT":
+            self.snake_position[1] -= 10
 
 # window_x = 720
 # window_y = 480
--- main.py
+++ main.py
@@ -1,79 +1,153 @@
 import random
-
 import pygame
-
 from Snake import GameLogics
 
 
-GAME_RUNNING = True
-
-class MainWindow(GameLogics):
+class MainWindow:
     def __init__(self):
-        super().__init__()
         self.WINDOW_X = 720
         self.WINDOW_Y = 480
-
+        self.game_window = None
+        self.game = None
+        self.current_screen = "menu"
+        self.clock = pygame.time.Clock()
 
     def init_window(self):
-        game_window = pygame.display.set_mode((self.WINDOW_X, self.WINDOW_Y))
+        self.game_window = pygame.display.set_mode((self.WINDOW_X, self.WINDOW_Y))
+        return self.game_window
 
     def start_game(self):
         pygame.init()
         pygame.display.set_caption('A snake from a small indie company')
+        self.game = GameLogics(self.WINDOW_X, self.WINDOW_Y)
         self.init_window()
         # fps = pygame.time.Clock()  # FPS
 
-    def change_snake_direction(self):
-        change_to = GameLogics().direction
+    def show_menu(self):
+        self.game_window.fill((0, 0, 0))
+        font = pygame.font.SysFont('Arial', 50)
+        title = font.render('Змейка', True, (255, 255, 255))
+        start = font.render('1. Начать игру', True, (255, 255, 255))
+        exit_text = font.render('2. Выход', True, (255, 255, 255))
 
-    def clear_screen(self):
-        self.init_window().fill(GameLogics().map_color)
-
-if __name__ == "main":
-    while GAME_RUNNING:
-        main_window = MainWindow()
-        main_window.start_game()
-
-        fps = pygame.time.Clock()  # FPS
-
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
-                    change_to = 'UP'
-                if event.key == pygame.K_DOWN:
-                    change_to = 'DOWN'
-                if event.key == pygame.K_LEFT:
-                    change_to = 'LEFT'
-                if event.key == pygame.K_RIGHT:
-                    change_to = 'RIGHT'
-            if main_window.change_snake_direction() == 'UP' and main_window.change_snake_direction() != 'DOWN':  # Запрещаю поворачивать в противоположную сторону
-                direction = 'UP'
-            if main_window.change_snake_direction() == 'DOWN' and main_window.change_snake_direction() != 'UP':
-                direction = 'DOWN'
-            if main_window.change_snake_direction() == 'LEFT' and main_window.change_snake_direction() != 'RIGHT':
-                direction = 'LEFT'
-            if main_window.change_snake_direction() == 'RIGHT' and main_window.change_snake_direction() != 'LEFT':
-                direction = 'RIGHT'
-            if main_window.change_snake_direction() == 'UP':
-                GameLogics().snake_position[1] -= 10
-            if main_window.change_snake_direction() == 'DOWN':
-                GameLogics().snake_position[1] += 10
-            if main_window.change_snake_direction() == 'LEFT':
-                GameLogics().snake_position[0] -= 10
-            if main_window.change_snake_direction() == 'RIGHT':
-                GameLogics().snake_position[0] += 10
-
-            if event.type == pygame.QUIT:
-                GAME_RUNNING = False
-
-            if not GameLogics().fruit_spawn:
-                mouse_position = [random.randrange(1, (main_window.WINDOW_X // 10)) * 10,
-                                  random.randrange(1, (main_window.WINDOW_Y // 10)) * 10]
-            fruit_spawn = True
-
-        GameLogics().show_score(GameLogics().SCORE_COLOR, 'consolas', GameLogics.SIZE_COLOR)
+        self.game_window.blit(title, (self.WINDOW_X // 2 - title.get_width() // 2, 100))
+        self.game_window.blit(start, (self.WINDOW_X // 2 - start.get_width() // 2, 200))
+        self.game_window.blit(exit_text, (self.WINDOW_X // 2 - exit_text.get_width() // 2, 260))
         pygame.display.update()
-        fps.tick(GameLogics().snake_speed)
 
-    else:
-        quit()
+    def run(self):
+        self.start_game()
+
+        while True:
+            if self.current_screen == "menu":
+                self.show_menu()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.current_screen = "exit"
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_1:
+                            self.current_screen = "game"
+                            self.game = GameLogics(self.WINDOW_X, self.WINDOW_Y)
+                        elif event.key == pygame.K_2:
+                            self.current_screen = "exit"
+
+            elif self.current_screen == "game":
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        self.current_screen = "exit"
+                    if event.type == pygame.KEYDOWN:
+                        if event.key == pygame.K_UP:
+                            self.game.change_to = 'UP'
+                        if event.key == pygame.K_DOWN:
+                            self.game.change_to = 'DOWN'
+                        if event.key == pygame.K_LEFT:
+                            self.game.change_to = 'LEFT'
+                        if event.key == pygame.K_RIGHT:
+                            self.game.change_to = 'RIGHT'
+
+                self.game.update_position()
+
+                self.game_window.fill(self.game.map_color)
+                self.game.render_snake(self.game_window)
+                self.game.render_mouse(self.game_window)
+                self.game.show_score(self.game_window, self.game.SCORE_COLOR, 'consolas', self.game.SIZE_COLOR)
+
+                self.game.eat_mouse()
+
+                if not self.game.fruit_spawn:
+                    self.game.mouse_position = [
+                        random.randrange(1, (self.WINDOW_X // 10)) * 10,
+                        random.randrange(1, (self.WINDOW_Y // 10)) * 10
+                    ]
+                    self.game.fruit_spawn = True
+
+                if self.game.suicide() or self.game.snake_translation():
+                    self.current_screen = self.game.game_over(self.game_window)
+
+                pygame.display.update()
+                self.clock.tick(self.game.snake_speed)
+
+            elif self.current_screen == "exit":
+                pygame.quit()
+                return
+
+
+if __name__ == "__main__":
+    game = MainWindow()
+    game.run()
+
+# if __name__ == "main":
+#     while GAME_RUNNING:
+#         main_window = MainWindow()
+#         main_window.start_game()
+#
+#         fps = pygame.time.Clock()  # FPS
+#
+#         for event in pygame.event.get():
+#             if event.type == pygame.KEYDOWN:
+#                 if event.key == pygame.K_UP:
+#                     change_to = 'UP'
+#                 if event.key == pygame.K_DOWN:
+#                     change_to = 'DOWN'
+#                 if event.key == pygame.K_LEFT:
+#                     change_to = 'LEFT'
+#                 if event.key == pygame.K_RIGHT:
+#                     change_to = 'RIGHT'
+#             if main_window.change_snake_direction() == 'UP' and main_window.change_snake_direction() != 'DOWN':  # Запрещаю поворачивать в противоположную сторону
+#                 direction = 'UP'
+#             if main_window.change_snake_direction() == 'DOWN' and main_window.change_snake_direction() != 'UP':
+#                 direction = 'DOWN'
+#             if main_window.change_snake_direction() == 'LEFT' and main_window.change_snake_direction() != 'RIGHT':
+#                 direction = 'LEFT'
+#             if main_window.change_snake_direction() == 'RIGHT' and main_window.change_snake_direction() != 'LEFT':
+#                 direction = 'RIGHT'
+#             if main_window.change_snake_direction() == 'UP':
+#                 GameLogics().snake_position[1] -= 10
+#             if main_window.change_snake_direction() == 'DOWN':
+#                 GameLogics().snake_position[1] += 10
+#             if main_window.change_snake_direction() == 'LEFT':
+#                 GameLogics().snake_position[0] -= 10
+#             if main_window.change_snake_direction() == 'RIGHT':
+#                 GameLogics().snake_position[0] += 10
+#
+#             if event.type == pygame.QUIT:
+#                 GAME_RUNNING = False
+#
+#             if not GameLogics().fruit_spawn:
+#                 mouse_position = [random.randrange(1, (main_window.WINDOW_X // 10)) * 10,
+#                                   random.randrange(1, (main_window.WINDOW_Y // 10)) * 10]
+#             fruit_spawn = True
+#
+#         GameLogics().show_score(GameLogics().SCORE_COLOR, 'consolas', GameLogics.SIZE_COLOR)
+#         pygame.display.update()
+#         fps.tick(GameLogics().snake_speed)
+#
+#     else:
+#         quit()
+
+
+# def change_snake_direction(self):
+#     change_to = GameLogics().direction
+#
+# def clear_screen(self):
+#     self.init_window().fill(GameLogics().map_color)
